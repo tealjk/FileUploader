70148d7cfc9f49a1bc8075728c6ca43b01598a66
587852 <tealjk914@gmail.com>
2017-04-05 04:25:55 -0400

    first

--- fileupload/src/main/java/com/example/FileuploadApplication.java
+++ fileupload/src/main/java/com/example/FileuploadApplication.java
@@ -3,10 +3,17 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.fileuploadapp.UploadController;
+
+import java.io.File;
+import java.io.IOException;
+
+
 @SpringBootApplication
 public class FileuploadApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		new File(UploadController.uploaddir).mkdirs();
 		SpringApplication.run(FileuploadApplication.class, args);
 	}
 }
--- fileupload/src/main/java/com/example/fileuploadapp/UploadController.java
+++ fileupload/src/main/java/com/example/fileuploadapp/UploadController.java
@@ -1,24 +1,46 @@
 package com.example.fileuploadapp;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import java.io.File;
+import java.io.IOException;
 
+
+
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 
 @RestController 
 public class UploadController {
+    public static final String uploaddir = System.getProperty("user.dir") + "/uploaddir/";
+
 	
 	
 	
-	@RequestMapping("/file")
-	public String fileLoader()
+	
+	@RequestMapping("/")
+	public String fileloader(Model model)
 	{	
-		return "file"; 
+		File file = new File(uploaddir);
+		model.addAttribute("files", file.listFiles());
+	
+		return "fileloader MEEEEE"; 
 		
 	}
 	
-	
-	
-	
+	@RequestMapping(value ="/", method = RequestMethod.POST)
+	public String uploading(@RequestParam("uploadingFiles") MultipartFile[] uploadingFiles) throws IOException{
+		
+    for(MultipartFile uploadedFile : uploadingFiles) {
+        File file = new File(uploading(null) + uploadedFile.getOriginalFilename());
+        uploadedFile.transferTo(file);
+    }
 
+    return "redirect:/";
+	
+	}
 }
